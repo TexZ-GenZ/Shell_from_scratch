9dfec396a6454d6a7f42f5316466840e6d258b89
TexZ-GenZ <priyanshusamal21@gmail.com>
2026-05-30 23:08:23 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,108 +1,13 @@
-import sys
 import os
 import subprocess
 import readline
 
 COMMANDS = {}
 
-
-def parser(text):
-    args = []
-    current = ""
-
-    i = 0
-    while i < len(text):
-        if text[i] == "'":
-            i += 1
-            while i < len(text) and text[i] != "'":
-                current += text[i]
-                i += 1
-        elif text[i] == '"':
-            i += 1
-            while i < len(text) and text[i] != '"':
-                if text[i] == "\\" and text[i + 1] in ['"', "\\"]:
-                    i += 1
-                current += text[i]
-                i += 1
-        elif text[i] == "\\":
-            i += 1
-            current += text[i]
-
-        elif text[i] == " ":
-            if current:
-                args.append(current)
-                current = ""
-
-            while i < len(text) and text[i] == " ":
-                i += 1
-            i -= 1
-        else:
-            current += text[i]
-
-        i += 1
-
-    if current:
-        args.append(current)
-
-    return args
-
-
-def redirect(text, redirect_type, file_path=None):
-    if redirect_type == "stdout" and file_path:
-        if text is not None:
-            with open(file_path, "w") as file:
-                file.write(text + "\n")
-        return
-
-    if redirect_type == "stdout_a" and file_path:
-        if text is not None:
-            with open(file_path, "a") as file:
-                file.write(text + "\n")
-        return
-
-    if redirect_type == "stderr" and file_path:
-        with open(file_path, "w"):
-            pass
-
-    if redirect_type == "stderr_a" and file_path:
-        with open(file_path, "a"):
-            pass
-
-    if text is not None:
-        print(text)
-
-
-def sanitize(command):
-    return [
-        ">" if token == "1>" else ">>" if token == "1>>" else token for token in command
-    ]
-
-
-def check_redirect(command):
-    if "2>>" in command:
-        idx = command.index("2>>")
-        return command[:idx], "stderr_a", " ".join(command[idx + 1 :])
-
-    elif ">>" in command:
-        idx = command.index(">>")
-        return command[:idx], "stdout_a", " ".join(command[idx + 1 :])
-
-    elif "2>" in command:
-        idx = command.index("2>")
-        return command[:idx], "stderr", " ".join(command[idx + 1 :])
-
-    elif ">" in command:
-        idx = command.index(">")
-        return command[:idx], "stdout", " ".join(command[idx + 1 :])
-
-    return command, None, None
-
-
 class shell_builtins:
     MEMBERS = ["echo", "exit", "type", "pwd", "cd", "complete", "jobs"]
     COMPLETION_SPEC = {}
     JOBS = {}
-    JOB_COUNTER = 1
 
     def __init__(self, command):
         self.exec = command[0]
@@ -199,6 +104,94 @@ class shell_builtins:
                 print(f"[{key}]{'+' if key == highest else '-' if key == second else ' '}  Done                 {val[1][:-1]}")
                 shell_builtins.JOBS.pop(key, None)
 
+def parser(text):
+    args = []
+    current = ""
+
+    i = 0
+    while i < len(text):
+        if text[i] == "'":
+            i += 1
+            while i < len(text) and text[i] != "'":
+                current += text[i]
+                i += 1
+        elif text[i] == '"':
+            i += 1
+            while i < len(text) and text[i] != '"':
+                if text[i] == "\\" and text[i + 1] in ['"', "\\"]:
+                    i += 1
+                current += text[i]
+                i += 1
+        elif text[i] == "\\":
+            i += 1
+            current += text[i]
+
+        elif text[i] == " ":
+            if current:
+                args.append(current)
+                current = ""
+
+            while i < len(text) and text[i] == " ":
+                i += 1
+            i -= 1
+        else:
+            current += text[i]
+
+        i += 1
+
+    if current:
+        args.append(current)
+
+    return args
+
+def redirect(text, redirect_type, file_path=None):
+    if redirect_type == "stdout" and file_path:
+        if text is not None:
+            with open(file_path, "w") as file:
+                file.write(text + "\n")
+        return
+
+    if redirect_type == "stdout_a" and file_path:
+        if text is not None:
+            with open(file_path, "a") as file:
+                file.write(text + "\n")
+        return
+
+    if redirect_type == "stderr" and file_path:
+        with open(file_path, "w"):
+            pass
+
+    if redirect_type == "stderr_a" and file_path:
+        with open(file_path, "a"):
+            pass
+
+    if text is not None:
+        print(text)
+
+def sanitize(command):
+    return [
+        ">" if token == "1>" else ">>" if token == "1>>" else token for token in command
+    ]
+
+def check_redirect(command):
+    if "2>>" in command:
+        idx = command.index("2>>")
+        return command[:idx], "stderr_a", " ".join(command[idx + 1 :])
+
+    elif ">>" in command:
+        idx = command.index(">>")
+        return command[:idx], "stdout_a", " ".join(command[idx + 1 :])
+
+    elif "2>" in command:
+        idx = command.index("2>")
+        return command[:idx], "stderr", " ".join(command[idx + 1 :])
+
+    elif ">" in command:
+        idx = command.index(">")
+        return command[:idx], "stdout", " ".join(command[idx + 1 :])
+
+    return command, None, None
+
 def completer(text, state):
     line = readline.get_line_buffer()
     words = line.split()
@@ -293,12 +286,12 @@ def run_builtin(parsed_command, redirect_type, file_path):
 
 def run_external(parsed_command, redirect_type, file_path, command_type):
     if command_type == "background":
+        i = 1
+        while i in shell_builtins.JOBS:
+            i += 1
         proc = subprocess.Popen(parsed_command)
-        counter = shell_builtins.JOB_COUNTER
-        val = proc
-        shell_builtins.JOBS[counter] = [val, " ".join(parsed_command) + " &"]
-        shell_builtins.JOB_COUNTER += 1
-        print(f"[{counter}] {val.pid}")
+        shell_builtins.JOBS[i] = [proc, " ".join(parsed_command) + " &"]
+        print(f"[{i}] {proc.pid}")
         return
     
     if redirect_type == "stdout":
