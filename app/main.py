209c8c1fb67cfe8c7ab83d9bbe5f5edd8e45f51d
TexZ-GenZ <priyanshusamal21@gmail.com>
2026-05-30 15:30:48 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,243 @@
-import shell_state
-from shell_builtins import shell_builtins
-from shell_commands import build_commands
-from shell_completion import setup_readline
-from shell_parse import parse_input
-from shell_runner import run_builtin, run_external
+import sys
+import os
+import subprocess
+import readline
+
+COMMANDS = {}
+
+
+def parser(text):
+    args = []
+    current = ""
+
+    i = 0
+    while i < len(text):
+        if text[i] == "'":
+            i += 1
+            while i < len(text) and text[i] != "'":
+                current += text[i]
+                i += 1
+        elif text[i] == '"':
+            i += 1
+            while i < len(text) and text[i] != '"':
+                if text[i] == "\\" and text[i + 1] in ['"', "\\"]:
+                    i += 1
+                current += text[i]
+                i += 1
+        elif text[i] == "\\":
+            i += 1
+            current += text[i]
+
+        elif text[i] == " ":
+            if current:
+                args.append(current)
+                current = ""
+
+            while i < len(text) and text[i] == " ":
+                i += 1
+            i -= 1
+        else:
+            current += text[i]
+
+        i += 1
+
+    if current:
+        args.append(current)
+
+    return args
+
+
+def redirect(text, redirect_type, file_path=None):
+    if redirect_type == "stdout" and file_path:
+        if text is not None:
+            with open(file_path, "w") as file:
+                file.write(text + "\n")
+        return
+
+    if redirect_type == "stdout_a" and file_path:
+        if text is not None:
+            with open(file_path, "a") as file:
+                file.write(text + "\n")
+        return
+
+    if redirect_type == "stderr" and file_path:
+        with open(file_path, "w"):
+            pass
+
+    if redirect_type == "stderr_a" and file_path:
+        with open(file_path, "a"):
+            pass
+
+    if text is not None:
+        print(text)
+
+
+def sanitize(command):
+    return [
+        ">" if token == "1>" else ">>" if token == "1>>" else token for token in command
+    ]
+
+
+def check_redirect(command):
+    if "2>>" in command:
+        idx = command.index("2>>")
+        return command[:idx], "stderr_a", " ".join(command[idx + 1 :])
+
+    elif ">>" in command:
+        idx = command.index(">>")
+        return command[:idx], "stdout_a", " ".join(command[idx + 1 :])
+
+    elif "2>" in command:
+        idx = command.index("2>")
+        return command[:idx], "stderr", " ".join(command[idx + 1 :])
+
+    elif ">" in command:
+        idx = command.index(">")
+        return command[:idx], "stdout", " ".join(command[idx + 1 :])
+
+    return command, None, None
+
+
+class shell_builtins:
+    MEMBERS = ["echo", "exit", "type", "pwd", "cd", "complete"]
+
+    def __init__(self, command):
+        self.exec = command[0]
+        self.args = command[1:]
+
+    def run(self):
+        match self.exec:
+            case "echo":
+                return self.echo()
+
+            case "pwd":
+                return self.pwd()
+
+            case "cd":
+                return self.cd()
+
+            case "type":
+                return self.type()
+
+    def echo(self):
+        return " ".join(self.args)
+
+    def pwd(self):
+        return os.getcwd()
+
+    def cd(self):
+        to_path = " ".join(self.args)
+
+        if to_path == "~":
+            os.chdir(os.environ["HOME"])
+
+        elif os.path.isdir(to_path):
+            os.chdir(to_path)
+
+        else:
+            return f"cd: {to_path}: No such file or directory"
+
+    def type(self):
+        arg = " ".join(self.args)
+
+        if arg in shell_builtins.MEMBERS:
+            return f"{arg} is a shell builtin"
+
+        if arg in COMMANDS:
+            return f"{arg} is {COMMANDS[arg]}"
+
+        return f"{arg}: not found"
+
+
+def completer(text, state):
+    line = readline.get_line_buffer()
+    words = line.split()
+    if len(words) == 0 or (len(words) == 1 and not line.endswith(" ")):
+        candidates = list(COMMANDS.keys()) + shell_builtins.MEMBERS
+        matches = [x for x in candidates if x.startswith(text)]
+        if state < len(matches):
+            return matches[state] + " "
+    else:
+        idx = text.rfind("/")
+        if idx == -1:
+            dir_path = "."
+            prefix = text
+        else:
+            dir_path = text[: idx + 1]
+            if idx < len(text) - 1:
+                prefix = text[idx + 1 :]
+            else:
+                prefix = ""
+
+        matches = []
+        for f in os.listdir(dir_path):
+            if f.startswith(prefix):
+                full = os.path.join(dir_path, f)
+                if os.path.isdir(full):
+                    matches.append(text[: idx + 1] + f + "/")
+                else:
+                    matches.append(text[: idx + 1] + f + " ")
+
+        if state < len(matches):
+            return matches[state]
+        return None
+
+    return None
+
+
+def build_commands():
+    commands = {}
+    seen = set()
+    for dir in os.environ.get("PATH", "").split(os.pathsep):
+        if dir in seen or not os.path.isdir(dir):
+            continue
+        seen.add(dir)
+        for entry in os.scandir(dir):
+            if entry.is_file() and os.access(entry.path, os.X_OK):
+                commands[entry.name] = entry.path
+    return commands
+
+
+def setup_readline():
+    readline.set_completer_delims(" \t\n")
+    readline.set_completer(completer)
+    readline.parse_and_bind("tab: complete")
+
+
+def parse_input(command):
+    parsed_command = sanitize(parser(command))
+    return check_redirect(parsed_command)
+
+
+def run_builtin(parsed_command, redirect_type, file_path):
+    out = shell_builtins(parsed_command).run()
+    redirect(out, redirect_type, file_path)
+
+
+def run_external(parsed_command, redirect_type, file_path):
+    if redirect_type == "stdout":
+        with open(file_path, "w") as file:
+            subprocess.run(parsed_command, stdout=file)
+
+    elif redirect_type == "stderr":
+        with open(file_path, "w") as file:
+            subprocess.run(parsed_command, stderr=file)
+
+    elif redirect_type == "stdout_a":
+        with open(file_path, "a") as file:
+            subprocess.run(parsed_command, stdout=file)
+
+    elif redirect_type == "stderr_a":
+        with open(file_path, "a") as file:
+            subprocess.run(parsed_command, stderr=file)
+
+    else:
+        subprocess.run(parsed_command)
+
 
 def main():
-    shell_state.COMMANDS = build_commands()
-    COMMANDS = shell_state.COMMANDS
+    global COMMANDS
+    COMMANDS = build_commands()
     setup_readline()
 
     while True:
